4b1a1e371ae712014d50d2bc782a7ef67aa93bd9
Jakub Arnold <darthdeus@gmail.com>
2014-11-17 13:21:09 +0100

    static > not static :bomb:

--- bfs.cpp
+++ bfs.cpp
@@ -17,17 +17,17 @@ struct color {
   explicit color(vertex_color c) : c(c) {}
 };
 
-color white(vertex_color::white);
-color gray(vertex_color::gray);
-color black(vertex_color::black);
+static color white(vertex_color::white);
+static color gray(vertex_color::gray);
+static color black(vertex_color::black);
 
-void color_white(vertex* v) { v->metadata = &white; }
-void color_gray(vertex* v) { v->metadata = &gray; }
-void color_black(vertex* v) { v->metadata = &black; }
+static void color_white(vertex* v) { v->metadata = &white; }
+static void color_gray(vertex* v) { v->metadata = &gray; }
+static void color_black(vertex* v) { v->metadata = &black; }
 
-bool is_white(vertex* v) { return ((color*)v->metadata) == &white; }
-bool is_gray(vertex* v) { return ((color*)v->metadata) == &gray; }
-bool is_black(vertex* v) { return ((color*)v->metadata) == &black; }
+static bool is_white(vertex* v) { return ((color*)v->metadata) == &white; }
+static bool is_gray(vertex* v) { return ((color*)v->metadata) == &gray; }
+static bool is_black(vertex* v) { return ((color*)v->metadata) == &black; }
 
 int bfs(graph& g, vertex* start, vertex* end) {
   for (auto& v : g.list) {
